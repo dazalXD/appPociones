a12f43be687aa65571449854c6acff17a599c46f
Dazz <ofter3132@gmail.com>
2025-12-11 12:27:37 -0600

    fix color de fondo

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -71,7 +71,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // --- Edge-to-Edge ---
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
     }
--- app/src/main/java/com/example/myapplication/Services/RetrofitClient.kt
+++ app/src/main/java/com/example/myapplication/Services/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private val URL_BASE = "http://10.55.37.2:3000/api/v1/"
+    private val URL_BASE = "http://192.168.1.105:3000/api/v1/"
 
     val apiService: ApiServices by lazy {
         Retrofit.Builder()
